e2366b7d8a93cf9983804654cde252ba576bab5f
Jose Boschero <113553694+joseboschero@users.noreply.github.com>
2024-01-04 03:34:46 -0300

    Update signUp.js

--- frontend/src/services/signUp.js
+++ frontend/src/services/signUp.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const signUpBaseUrl = "http://localhost:3001/users";
+const signUpBaseUrl = "https://notes-app-backend-tmv2.onrender.com/users";
 
 const signUp = async (credentials) => {
   const { data } = await axios.post(signUpBaseUrl, credentials);
